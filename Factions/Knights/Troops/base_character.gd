608df0d93f396c89d73c8c213652589e93d5229d
brendon-joao-dev <brendonjoaodev@gmail.com>
2026-08-28 20:09:31 -0300

    Dia004: animações básicas

--- Factions/Knights/Troops/base_character.gd
+++ Factions/Knights/Troops/base_character.gd
@@ -3,6 +3,10 @@ class_name BaseCharacter
 @export_category("Variables")
 @export var _move_speed: float = 128.0
 
+@export_category("Objects")
+@export var _sprite2D: Sprite2D
+@export var _animation: AnimationPlayer
+
 func _process(delta: float) -> void:
 	pass
 	
@@ -12,6 +16,19 @@ func _physics_process(delta: float) -> void:
 	)
 	velocity = _direction * _move_speed
 	move_and_slide()
+	
+	if velocity.x > 0:
+		_sprite2D.flip_h = false
+		
+	if velocity.x < 0:
+		_sprite2D.flip_h = true
+		
+	if velocity:
+		_animation.play("run")
+		return
+		
+	_animation.play("idle")
+	
 	print(_direction)
 	print(_direction.length())
 	pass
