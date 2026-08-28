ab070ace191c60775deca1fcfcfe6f6974cc503a
brendon-joao-dev <brendonjoaodev@gmail.com>
2026-08-28 20:28:38 -0300

    Dia004: ataques - início

--- Factions/Knights/Troops/base_character.gd
+++ Factions/Knights/Troops/base_character.gd
@@ -9,26 +9,30 @@ class_name BaseCharacter
 
 func _process(delta: float) -> void:
 	pass
-	
+
 func _physics_process(delta: float) -> void:
+	_move()
+	_animate()
+
+func _move() -> void:
 	var _direction: Vector2 = Input.get_vector(
 		"move_left", "move_right", "move_up", "move_down"
 	)
 	velocity = _direction * _move_speed
 	move_and_slide()
-	
+
+func _animate() -> void:
 	if velocity.x > 0:
 		_sprite2D.flip_h = false
-		
+
 	if velocity.x < 0:
 		_sprite2D.flip_h = true
-		
+
 	if velocity:
 		_animation.play("run")
 		return
-		
+
 	_animation.play("idle")
-	
-	print(_direction)
-	print(_direction.length())
-	pass
+
+# 	print(_direction)
+# 	print(_direction.length())
