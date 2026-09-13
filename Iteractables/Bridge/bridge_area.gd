6f1b9fc55378042f270692cee29a1443db4f7a5c
brendon-joao-dev <brendonjoaodev@gmail.com>
2026-09-12 22:47:20 -0300

    Dia019: porque pontes são tão malignas?

--- Iteractables/Bridge/bridge_area.gd
+++ Iteractables/Bridge/bridge_area.gd
@@ -1,9 +1,6 @@
 extends Area2D
 class_name BridgeArea
 
-@export_category("Variables")
-@export var _bridge_orietation: String
-
 func _on_body_entered(_body: Node2D) -> void:
 	if _body is BaseCharacter:
 		if _body.get_is_in_bridge() == false:
@@ -13,7 +10,7 @@ func _on_body_entered(_body: Node2D) -> void:
 
 func _on_body_exited(_body: Node2D) -> void:
 	if _body is BaseCharacter:
-		if _body.get_is_in_mountain() == false:
+		if _body.get_is_in_bridge() == false:
 			return
 		
 		_body.update_collision_layer_mask("out")
--- Iteractables/Bridge/bridge_transition.gd
+++ Iteractables/Bridge/bridge_transition.gd
@@ -1,15 +1,51 @@
 extends Area2D
 class_name BridgeTransition
 
-@onready var collision_shape: CollisionShape2D = get_node("CollisionShape2D")
+@onready var collision_shape: CollisionShape2D = get_node("Collision")
 # Usar $nome_nó é o mesmo que o get_node acima
+
+@export_category("Variables")
+@export var _bridge_orietation: bool = false # true = vertical; false = horizontal; 
+@export var _bridge_is_reverse: bool = false # true = voltando; false = indo;
 
 func _on_body_exited(_body: Node2D) -> void:
 	if _body is BaseCharacter:
-		if  collision_shape.global_position.y > _body.global_position.y:
-			_body.update_bridge_state(true)
-		if collision_shape.global_position.y < _body.global_position.y:
-			_body.update_bridge_state(false)
+		# ponte vertical
+		if _bridge_orietation == true:
+			# indo
+			if _bridge_is_reverse == false:
+				if _body.global_position.y > collision_shape.global_position.y:
+					_body.update_bridge_state(true)
+					
+				if _body.global_position.y < collision_shape.global_position.y:
+					_body.update_bridge_state(false)
+			
+			# voltando
+			if _bridge_is_reverse == true:
+				pass
+			pass
+			
+		# ponte horizontal
+		if _bridge_orietation == false:
+			# indo
+			if _bridge_is_reverse == false:
+				pass
+			
+			# voltando
+			if _bridge_is_reverse == true:
+				pass
+			pass
+			
+
+			#if  collision_shape.global_position.y < _body.global_position.y:
+				#_body.update_bridge_state(true)
+			#if collision_shape.global_position.y > _body.global_position.y:
+				#_body.update_bridge_state(false)
+				#
+			#if  collision_shape.global_position.y > _body.global_position.y:
+				#_body.update_bridge_state(true)
+			#if collision_shape.global_position.y < _body.global_position.y:
+				#_body.update_bridge_state(false)
 
 func _on_body_entered(_body: Node2D) -> void:
 	if _body is BaseCharacter:
